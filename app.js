c4e9a11baaa5962f6d98e4d8553359148cd39b81
Steve P. Olson <36646456+stevepo74@users.noreply.github.com>
2024-04-30 17:52:41 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const grid = document.querySelector('.grid')
+const resultDisplay = document.querySelector('results')
+const width = 15
+
+for (i = 0; i < width * width; i++) {
+    const square = document.createElement('div')
+    grid.appendChild(square)
+}
