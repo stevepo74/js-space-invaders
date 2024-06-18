5e9dd8091517e00a1d1fe966f062c8bd3fba7219
Steve P. Olson <36646456+stevepo74@users.noreply.github.com>
2024-06-18 15:31:02 -0500

    add array block to js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const grid = document.querySelector('.grid')
-const resultDisplay = document.querySelector('results')
+const resultDisplay = document.querySelector('.results')
 const width = 15
 
 for (i = 0; i < width * width; i++) {
     const square = document.createElement('div')
     grid.appendChild(square)
 }
+
+const squares = Array.from(document.querySelectorAll('.grid div'))
+
+console.log(squares)
