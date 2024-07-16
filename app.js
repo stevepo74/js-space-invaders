e63700208d79845ef8a2d5fad5dc6ab4345e0b49
Steve P. Olson <36646456+stevepo74@users.noreply.github.com>
2024-07-16 14:01:34 -0500

    expand and add functions and add styling to basic elements

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const grid = document.querySelector('.grid')
 const resultDisplay = document.querySelector('.results')
 const width = 15
+const aliensRemoved = []
 
 for (i = 0; i < width * width; i++) {
     const square = document.createElement('div')
@@ -13,12 +14,14 @@ console.log(squares)
 
 const alienInvaders = [
     0,1,2,3,4,5,6,7,8,9,
-    15,16,17,18,19,20,21,22,23,24
+    15,16,17,18,19,20,21,22,23,24,
     30,31,32,33,34,35,36,37,38,39
 ]
 
 function draw() {
     for (let i = 0; i < alienInvaders.length; i++) {
-        
+        if (!aliensRemoved.includes(i)) {
+        squares[alienInvaders[i]].classList.add('invader')  
     }
 }
+draw()
